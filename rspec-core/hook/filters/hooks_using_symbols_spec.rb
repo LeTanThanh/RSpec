508a20abf2ccaa0398f43e1fbe205bc66dde73c4
LeTanThanh <tanthanhcr95@gmail.com>
2019-05-10 09:13:05 +0700

    fix

--- rspec-core/hook/filters/hooks_using_symbols_spec.rb
+++ rspec-core/hook/filters/hooks_using_symbols_spec.rb
@@ -8,26 +8,26 @@ RSpec.describe "a filter before :example hook" do
   let(:invoked_hooks) { [] }
 
   describe "group without a matching metadata key" do
-    it "does not run the hook"
-      expect(invoked_hook).to be_empty
+    it "does not run the hook" do
+      expect(invoked_hooks).to be_empty
     end
 
-    it "does not run the hook for an example with metadata hash containing the key with a falsey value", foo: :nil do
-      expect(invoked_hook).to be_empty
+    it "does not run the hook for an example with metadata hash containing the key with a falsey value", foo: nil do
+      expect(invoked_hooks).to be_empty
     end
 
     it "runs the hook for an example with metadata hash containing the key with a truthy value", foo: :bar do
-      expect(invoked_hook).to eq([:before_example_foo_bar])
+      expect(invoked_hooks).to eq([:before_example_foo_bar])
     end
 
     it "runs the hook for an example with only the key defined", :foo do
-      expect(invoked_hook).to eq([:before_example_foo_bar])
+      expect(invoked_hooks).to eq([:before_example_foo_bar])
     end
   end
 
   describe "group with matching metadata key", :foo do
     it "runs the hook" do
-      expect(invoked_hook).to eq([:before_example_foo_bar])
+      expect(invoked_hooks).to eq([:before_example_foo_bar])
     end 
   end
 end
